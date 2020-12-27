864c375a8b25927be3acd1776b02be1ae5a3a7b9
Andrei Sugeac <andreisugeac@gmail.com>
2020-12-28 00:00:53 +0200

    First commit

---
+++ Models/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketLine.Models
+{
+    public class Ticket
+    {
+
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public Ticket()
+        {
+        }
+    }
+}
